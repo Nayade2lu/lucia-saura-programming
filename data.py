7019fd4af33622f772afca3e17c211442aa0519d
L S <lucia@Ls-MacBook-Air.local>
2018-03-19 21:18:52 +0000

    Iris data formatting (not working)

--- data.py
+++ data.py
@@ -1,8 +1,9 @@
 #Format Iris data Lucia Saura 19/03/2018
-# working on formating Iris data (not working, error Unknown format code 'd' for object of type 'str')
+# working on formating Iris data (not working, error <_io.TextIOWrapper name='irisdata.csv' mode='r' encoding='UTF-8'>)
 
 with open("irisdata.csv") as f:
     contents = f.read()
-    print ('{:2d} {} {} {} {}'.format(contents))
+    print(repr(f).rjust(2))
+    
 
 print ("out of with")
