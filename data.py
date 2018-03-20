59d9c480623ddd4a3f5c1ef2529dbf32f11a99bf
L S <lucia@Ls-MacBook-Air.local>
2018-03-20 21:21:55 +0000

    Iris data

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 #Open Iris data Lucia Saura 20/03/2018
-# iris data set split in columns (from SARAH SCHOLZ code)
-# Now I am ony missing  how to print the text (strings)
+# iris data set split in columns (based on SARAH SCHOLZ code)
+# 
 
 with open("irisdata.csv") as f:
-    for line in f:
-        print('{:5} {:5} {:5} {:5}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
+    for line in f: #loops trough each line
+        print('{:6} {:6} {:6} {:6}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]), line.split(',')[4])
         # first bit are the spaces between columns, second bit spliting and formating every colum
+        # added the last part to show the text column as well
        
         
 
