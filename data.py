35f38deb2c24298f5e76d8e149322bf5af2c3517
L S <lucia@Ls-MacBook-Air.local>
2018-03-20 20:59:52 +0000

    iris data split in rows

--- data.py
+++ data.py
@@ -1,11 +1,9 @@
 #Open Iris data Lucia Saura 20/03/2018
-# iris data set split in lines
+# iris data set split in columns (from KENNETH MC TIGUE code)
 
 with open("irisdata.csv") as f:
     for line in f:
-        print(line)
-        line_split=line.split(',')
-        print(line.split)
+        print ("{:>12} {:>12} {:>13} {:>12}" .format(line.split(',')[0],line.split(',')[1],line.split(',')[2],line.split(',')[3]))
        
         
 
