cbb83090a10d832f16972dedb7b7ecee85b284c4
L S <lucia@Ls-MacBook-Air.local>
2018-03-20 21:31:36 +0000

    iris data set (without spaces between lines)

--- data.py
+++ data.py
@@ -4,9 +4,10 @@
 
 with open("irisdata.csv") as f:
     for line in f: #loops trough each line
-        print('{:6} {:6} {:6} {:6}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]), line.split(',')[4])
+        print('{:6} {:6} {:6} {:6}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]), line.split(',')[4], end='')
         # first bit are the spaces between columns, second bit spliting and formating every colum
         # added the last part to show the text column as well
+        # , end='' removes the blanc new line between lines
        
         
 
