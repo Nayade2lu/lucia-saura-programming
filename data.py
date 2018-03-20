4bf213311adfdd1f448c7830cd6a4a4e9f5c8d50
L S <lucia@Ls-MacBook-Air.local>
2018-03-20 21:06:48 +0000

    Iris data formating numbers (working)

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
 #Open Iris data Lucia Saura 20/03/2018
-# iris data set split in columns (from KENNETH MC TIGUE code)
+# iris data set split in columns (from SARAH SCHOLZ code)
+# Now I am ony missing  how to print the text (strings)
 
 with open("irisdata.csv") as f:
     for line in f:
-        print ("{:>12} {:>12} {:>13} {:>12}" .format(line.split(',')[0],line.split(',')[1],line.split(',')[2],line.split(',')[3]))
+        print('{:5} {:5} {:5} {:5}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
+        # first bit are the spaces between columns, second bit spliting and formating every colum
        
         
 
