38994f738dc55033b5f0c3e773612eff5091c6ae
Nayade2lu <36339477+Nayade2lu@users.noreply.github.com>
2018-03-24 14:10:24 +0000

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -1,38 +1,14 @@
-# Lucia Saura 19/03/2018
-# Attempting to run the code proposed in the Euler page adding the Prime numbers each one of them for a p[] position
-# https://learnonline.gmit.ie/pluginfile.php/344250/mod_resource/content/1/euler-5-overview.pdf
-# Code seem to not be python
-k = 20
-N = 1
-i = 1
-p[1]=2
-p[2]=3
-p[3]=5
-p[4]=7
-p[5]=11
-p[6]=13
-p[7]=17
-p[8]=19
-check = true
-limit = sqrt(k)
+# lucia saura 24/03/2018
+# first step euler 5
+# identifying prime numbers but not fully working because I need a list instead a single value for a (now says that any number not divisible by two is prime)
 
-    while p[i] <= k:
-        a[i] = 1
-        if check then
-            if p[i] <= limit then
-                a[i] = floor( log(k) / log(p[i]) )
-            else
-                check = false
-            end if
-        end if
-        N = N * p[i] ^ a[i]
-    i = i + 1
-end while 
-output N
-
-#Getting the same error as the code seem not to be python
-#Ls-MacBook-Air:Week 4_13_03_2018 lucia$ python Euler5v5.py
-  #File "Euler5v5.py", line 15
-    #while p[i] <= k:
-    #^
-#IndentationError: unexpected indent
+a=2
+num=24
+while num > a :
+  if num%a==0 & a!=num:
+    print('not prime')
+    break
+    a=a+1
+  else:
+    print('prime')
+    a=(num)+1
