4b2627ea9c3ebb7ef2adfdf6709d677e04bd38c8
Nayade2lu <36339477+Nayade2lu@users.noreply.github.com>
2018-03-24 10:38:44 +0000

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -1,14 +1,29 @@
-# Lucia Saura 13/3/2018 Euler 5
+# Lucia Saura 19/03/2018
+# Attempting to run the code proposed in the Euler page
+# https://learnonline.gmit.ie/pluginfile.php/344250/mod_resource/content/1/euler-5-overview.pdf
+# Code seem to not be python
+k = 20
+N = 1
+i = 1
+check = true
+limit = sqrt(k)
+while p[i]<= k
+    a[i] = 1
+    if check then
+        if p[i] <= limit then
+            a[i] = floor( log(k) / log(p[i]) )
+        else
+            check = false
+        end if
+    end if
+    N = N * p[i] ^ a[i]
+    i = i + 1
+end while
+output N
 
-for i in range (1,10):
-    if i % range == 0:
-        print(i)
-        
-# New attempt: still convinced that I shouldn't use the range in line 3... Trying with small numbers to see if it makes more simpler for me to understand
-#It doesn't work, I get an error... leaving it here from now, tomorrow more. I also made visual studio code stop with a very big loop using a range (0, 1000000000)...
-# >>>s-MacBook-Air:Week 4_13_03_2018 lucia$ python Euler5v3.py
-# Traceback (most recent call last):
-  # File "Euler5v3.py", line 2, in <module>
-    # if i % range == 0:
-# TypeError: unsupported operand type(s) for %: 'int' and 'type'
-# Ls-MacBook-Air:Week 4_13_03_2018 lucia$
+#Result: I get an error: 
+#Ls-MacBook-Air:Week 4_13_03_2018 lucia$ python Euler5v5.py
+  #File "Euler5v5.py", line 7
+    #while p[i] <= k
+    #^
+#IndentationError: unexpected indent
