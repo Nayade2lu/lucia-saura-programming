d6cedcea01c4a785b2347dd788a151212c1fb5a1
Nayade2lu <36339477+Nayade2lu@users.noreply.github.com>
2018-03-24 10:35:53 +0000

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -6,9 +6,9 @@ for i in range (1,10):
         
 # New attempt: still convinced that I shouldn't use the range in line 3... Trying with small numbers to see if it makes more simpler for me to understand
 #It doesn't work, I get an error... leaving it here from now, tomorrow more. I also made visual studio code stop with a very big loop using a range (0, 1000000000)...
->>>s-MacBook-Air:Week 4_13_03_2018 lucia$ python Euler5v3.py
-Traceback (most recent call last):
-  File "Euler5v3.py", line 2, in <module>
-    if i % range == 0:
-TypeError: unsupported operand type(s) for %: 'int' and 'type'
-Ls-MacBook-Air:Week 4_13_03_2018 lucia$
+# >>>s-MacBook-Air:Week 4_13_03_2018 lucia$ python Euler5v3.py
+# Traceback (most recent call last):
+  # File "Euler5v3.py", line 2, in <module>
+    # if i % range == 0:
+# TypeError: unsupported operand type(s) for %: 'int' and 'type'
+# Ls-MacBook-Air:Week 4_13_03_2018 lucia$
