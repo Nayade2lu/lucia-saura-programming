f5ced7536b9594c74ea63e63e5066dd1dcfdff33
Nayade2lu <36339477+Nayade2lu@users.noreply.github.com>
2018-03-24 19:20:22 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,12 +1,13 @@
 #lucia saura 24/03/2018
-#factorial 
+#factorial
 
-def factorial (x): 
+def factor (x):
     #first a name is given to the function and a value assigned
-    x = (x-1) * (x-2) * (x-3) * (x-4)
-    #here I actualize the number sustracting each time one number more and multiplying them
-    return x 
-# return the x   
-print (factorial (5))
-# and in the print statement I call the name of the function and give the value to the x
-#I have realized that this function is valid only to number 5
+    while x !=0:
+        #when x is greater than 0 do the following
+        x= x * (x-1)
+        # actualize the value of x for the result of x multiplied per x-1
+        #not working because the loop never gets to x=0 (the number is bigger and bigger each time and never ends)
+    return x
+print (factor (4))
+#not working as the loop never ends
