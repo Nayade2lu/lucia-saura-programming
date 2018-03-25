49dad79e9b26e968a12b64fcc07a79286ad633f7
Lucia Saura <nayade_2lu@hotmail.com>
2018-03-25 13:13:14 +0100

    factorial working ok

--- factorial.py
+++ factorial.py
@@ -1,19 +1,20 @@
 # Lucia Saura 24/03/2017
 # factorial
+# source of information https://www.youtube.com/watch?v=D6qpjk48U6U
 
-def factorial(y):
-    #naming the fucntion and the value I will call later
+def factorial3 (num):
+#first a name is given to the function and a name to the value that we will be calling at the end (when calling the function)
+#in the video source they are doing that asking a number on the shell (don't apply here) 
     x=1
-    # assigning a value to x
-    while x < y-1:
-    # loop provided that the x is less than y-1 (I thought the fatorial did not include the multiplication by itself, this is why I was substracting a 1 to the y)
-        x=x+1
-        #actualise the value of x each time (from 1 to y-1)
-        r= x * (x+1)
-        #show the result of the multiplication of the numbers I got in the loop
-    return r
-    #return the result
-print(factorial(5))
-#calling the function and giving y a value
-#not working it seems it only shows the result of the last multiplication ((x)4 * (x+1)5)
+# create a value and assigning to 1 to work with in the for loop
+    for i in range (num):
+    # loop in the range from 0 to the (num) 
+        x=x * num
+        # multiply the x for the number given and we obtain the number 
+        num = num -1
+        #then actualise the number for one less than itself and repeat the process in the for loop wilth all numbers less than the (num) until arriving to 1
+    print (x)
+    #print the final result of the loop (x)
+print(factorial3(10))
+# in this statement we call the function and assign a value to (num)
     
