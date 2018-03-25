05259dcb9250be39f0a815ce6ca66d40233bf561
Lucia Saura <nayade_2lu@hotmail.com>
2018-03-25 19:42:54 +0100

    euler5 working

--- euler5.py
+++ euler5.py
@@ -1,46 +1,20 @@
-# lucia saura 24/03/2018
-# first step euler 5
-# identifying prime numbers (code source python tutorial)
+#Lucia Saura 25/03/2018
+# euler 5 
+# source https://www.youtube.com/watch?v=EMTcsNMFS_g
 
+def euler5 (ni): #first create a function
+    for i in range (11,21): #loop trough the numbers from 11 to 21 (if it is divisible by 11 to 20 is also from 1 to 10)
+        if ni % i ==0: #if the number is divisible by the number in the range 11 to 21 (remainder0)
+            continue #the loops continues
+        else: #if it is not divisible 
+            return False #the program gives false (boolean)
+    return True #the program gives true (boolean)
 
-for n in range(2, 20):
-     for x in range(2, n):
-         if n % x == 0:
-             print(n, 'equals', x, '*', n//x)
-             break
-     else:
-         # loop fell through without finding a factor
-         print(n, 'is a prime number')
-         #attempt to multiply the prime numbers, not working: it is multiplying the prime number by itself
+x= 2520 #the problem gives us the minimum common multiple of the range 1 to 10 and here it is used to increase the number just on multiples of it
+while not euler5(x): #here the while loop keeps looping provided that the number is not 2520)
+    x +=2520 #and actualises the number only in multiples of 2510 what makes the program very efficient
+print (x) #this command prints the result of the problem
 
-
-def factorial3 (num):
-#first a name is given to the function and a name to the value that we will be calling at the end (when calling the function)
-#in the video source they are doing that asking a number on the shell (don't apply here) 
-    x=1
-# create a value and assigning to 1 to work with in the for loop
-    for i in range (num):
-    # loop in the range from 0 to the (num) 
-        x=x * num
-        # multiply the x for the number given and we obtain the number 
-        num = num -1
-        #then actualise the number for one less than itself and repeat the process in the for loop wilth all numbers less than the (num) until arriving to 1
-    print (x)
-    #print the final result of the loop (x)
-print(factorial3(20))
-# in this statement we call the function and assign a value to (num)
-# a 'None' appearing in the terminal after the result (To be investigated)  
-
-for nu in range (25200000,243290200817664):
-  # this loop sets the range from the minimum common divisor from 1 to 10 and the factorial of 20
-  for s in range (11,21):
-    # this loop sets the range of numbers from 1 to 20
-    if nu % s ==0:
-      #the if statement shows the numbers (from 2520,to 20 factorial) that are divisible from the numbers from 1 to 20
-      #prints all the numbers that are divisible (it doesn't stop in the first one)
-      break 
-      #the break is supposed to end the loop 
-      #not working because it prints lots of numbers that are divisible not only the first one
-      nu=nu+1
-print (nu)
+#for now this is the only way I have been able to make the program work in my code
+#it is very efficient. (advanced knowledge on true and false required) 
 
