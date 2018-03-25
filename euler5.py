924083df2303bc19abf3ac454d678904683eba71
Lucia Saura <nayade_2lu@hotmail.com>
2018-03-25 15:46:39 +0100

    prime numbers + factorial 20

--- euler5.py
+++ euler5.py
@@ -13,3 +13,21 @@ for n in range(2, 10):
          print(n * n, 'multiplication of prime numbers')
          #attempt to multiply the prime numbers, not working: it is multiplying the prime number by itself
 
+
+def factorial3 (num):
+#first a name is given to the function and a name to the value that we will be calling at the end (when calling the function)
+#in the video source they are doing that asking a number on the shell (don't apply here) 
+    x=1
+# create a value and assigning to 1 to work with in the for loop
+    for i in range (num):
+    # loop in the range from 0 to the (num) 
+        x=x * num
+        # multiply the x for the number given and we obtain the number 
+        num = num -1
+        #then actualise the number for one less than itself and repeat the process in the for loop wilth all numbers less than the (num) until arriving to 1
+    print (x)
+    #print the final result of the loop (x)
+print(factorial3(20))
+# in this statement we call the function and assign a value to (num)
+# a 'None' appearing in the terminal after the result (To be investigated)            
+
