42bc18e0a318a2849976947e100df9a16e806d00
Lucia Saura <nayade_2lu@hotmail.com>
2018-03-25 15:39:22 +0100

    prime numbers

--- euler5.py
+++ euler5.py
@@ -1,14 +1,13 @@
 # lucia saura 24/03/2018
 # first step euler 5
-# identifying prime numbers but not fully working because I need a list instead a single value for a (now says that any number not divisible by two is prime)
+# identifying prime numbers (code source python tutorial)
 
-a=2
-num=24
-while num > a :
-  if num%a==0 & a!=num:
-    print('not prime')
-    break
-    a=a+1
-  else:
-    print('prime')
-    a=(num)+2
+for n in range(2, 10):
+     for x in range(2, n):
+         if n % x == 0:
+             print(n, 'equals', x, '*', n//x)
+             break
+     else:
+         # loop fell through without finding a factor
+         print(n, 'is a prime number')
+
