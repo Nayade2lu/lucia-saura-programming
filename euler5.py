24984414713bf62872691984e136351ab17d6f7c
Lucia Saura <nayade_2lu@hotmail.com>
2018-03-25 15:48:39 +0100

    primes to 20 + factorial 20

--- euler5.py
+++ euler5.py
@@ -3,7 +3,7 @@
 # identifying prime numbers (code source python tutorial)
 
 
-for n in range(2, 10):
+for n in range(2, 20):
      for x in range(2, n):
          if n % x == 0:
              print(n, 'equals', x, '*', n//x)
